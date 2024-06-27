8bd936a235ef12658a3af52e79e9096b6adee698
gustavo <gustavo.nbispo@outlook.com>
2024-06-27 10:13:26 -0300

    Route Params

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello world!");
+app.get("/message/:id/:user", (request, response) => {
+  const { id, user } = request.params;
+  
+  response.send(`
+    Id da mensagem: ${id}. 
+    Para o usuário: ${user}.`);
 });
 
 const PORT = 3333;
