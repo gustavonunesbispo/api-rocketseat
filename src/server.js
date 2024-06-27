1b9ebe4887cdc447d449c13766013bfdaf0cc63d
gustavo <gustavo.nbispo@outlook.com>
2024-06-27 11:17:59 -0300

    Query Params

--- src/server.js
+++ src/server.js
@@ -4,10 +4,18 @@ const app = express();
 
 app.get("/message/:id/:user", (request, response) => {
   const { id, user } = request.params;
-  
+
   response.send(`
     Id da mensagem: ${id}. 
     Para o usuário: ${user}.`);
+});
+
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query;
+
+  response.send(`
+    Página: ${page}.
+    Mostrar: ${limit}`);
 });
 
 const PORT = 3333;
