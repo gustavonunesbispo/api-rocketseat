989001bdca0b3789a8452b181afb3e12dcb4851b
gustavo <gustavo.nbispo@outlook.com>
2024-06-27 16:09:28 -0300

    add controllers

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,11 @@
 const { Router } = require("express")
 
-const usersRoutes = Router()
+const UsersController = require("../controllers/UsersController");
 
-usersRoutes.post("/", (request, response) => {
-  const { name, email, password } = request.body
+const usersRoutes = Router();
 
-  response.json({ name, email, password });
-});
+const usersController = new UsersController();
+
+usersRoutes.post("/", usersController.create);
 
 module.exports = usersRoutes;
