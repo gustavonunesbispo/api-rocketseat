afacb911453251b3eadfa36b4aa5def9f87427e5
gustavo <gustavo.nbispo@outlook.com>
2024-08-01 11:30:37 -0300

    migrations for notes with Knex

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
-    }
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
+    useNullAsDefault: true
   },
 };
