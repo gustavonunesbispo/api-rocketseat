0c885ef3fe6aab2580460e0e7f0659d6eb5029bb
gustavo <gustavo.nbispo@outlook.com>
2024-08-07 11:32:03 -0300

    list tags

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -64,7 +64,16 @@ class NotesController{
       const filterTags = tags.split(',').map(tag => tag.trim());
 
       notes = await knex("tags")
-      .whereIn("name", filterTags);
+      .select([
+        "notes.id",
+        "notes.title",
+        "notes.user_id"
+      ])
+      .where("notes.user_id", user_id)
+      .whereLike("notes.title", `%${title}%`)
+      .whereIn("name", filterTags)
+      .innerJoin("notes", "notes.id", "tags.note_id")
+      .orderBy("notes.title")
     }else {    
       notes = await knex("notes")
       .where({ user_id })
@@ -72,7 +81,17 @@ class NotesController{
       orderBy("title");
     };
 
-    return response.json(notes)
+    const userTags = await knex("tags").where({ user_id });
+    const notesWithTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id);
+
+      return {
+        ...note,
+        tags: noteTags
+      }
+    })
+
+    return response.json(notesWithTags)
   }
 };
 
